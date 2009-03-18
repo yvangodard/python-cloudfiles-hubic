f1ad022c4109ced90987535f868743f944f9ad88
Jay Farrimond <jay.farrimond@rackspace.com>
2009-03-18 02:06:55 -0500

    freerange v1.3.0

--- freerange/consts.py
+++ freerange/consts.py
@@ -1,7 +1,7 @@
 """ See COPYING for license information. """
 from cloudfiles.consts import *
 
-__version__ = "1.0.0"
+__version__ = "1.3.0"
 user_agent = "Freerange/%s" % __version__
 default_api_version = 1
 default_authurl = 'https://api.mosso.com/'
