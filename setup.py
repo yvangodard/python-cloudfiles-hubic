9fe65031deee00b2ff594277a81ce1adedc0362a
Chmouel Boudjnah <chmouel.boudjnah@rackspace.co.uk>
2009-08-27 15:51:56 -0500

    Get the version from consts not setup statically.

--- setup.py
+++ setup.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python
 
 from distutils.core import setup
+from cloudfiles.consts import __version__
+
 setup(
     name='cloudfiles',
-    version='1.4.0',
+    version=__version__,
     description='CloudFiles client library for python',
     author='Racklabs',
     url='https://www.mosso.com/',
