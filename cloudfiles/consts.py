7c513c7b192af315bf225e4ba3372e31073c8f3f
Chmouel Boudjnah <chmouel.boudjnah@rackspace.co.uk>
2011-01-05 23:48:20 +0000

    1.7.5 Release and fixes.
    - Fix UK auth_url.
    - Add default_authurl or it will break duplicity.
    - Regenerate docs and such for release.

--- cloudfiles/consts.py
+++ cloudfiles/consts.py
@@ -1,9 +1,10 @@
 """ See COPYING for license information. """
 
-__version__ = "1.7.4"
+__version__ = "1.7.5"
 user_agent = "python-cloudfiles/%s" % __version__
 us_authurl = 'https://auth.api.rackspacecloud.com/v1.0'
-uk_authurl = 'https://auth.api.rackspacecloud.com/v1.0'
+uk_authurl = 'https://lon.auth.api.rackspacecloud.com/v1.0'
+default_authurl = us_authurl
 default_cdn_ttl = 86400
 cdn_log_retention = False
 cdn_acl_user_agent = ""
