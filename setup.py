8726b22f60db558290dd7f6a9f45f683e71f8ddb
BJ Dierkes <wdierkes@5dollarwhitebox.org>
2011-01-25 20:22:42 -0600

    Added setuptools support

--- setup.py
+++ setup.py
@@ -1,13 +1,23 @@
-#!/usr/bin/python
 
-from distutils.core import setup
+from setuptools import setup, find_packages
 from cloudfiles.consts import __version__
 
-setup(
-    name='cloudfiles',
+setup(name='cloudfiles',
     version=__version__,
-    description='CloudFiles client library for python',
+    description='CloudFiles client library for Python',
+    classifiers=[], 
+    keywords='',
     author='Rackspace',
+    author_email='',
     url='https://www.rackspacecloud.com/',
-    packages=['cloudfiles']
-)
+    license='MIT',
+    packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[],
+    setup_requires=[],
+    test_suite='nose.collector',
+    entry_points="""
+    """,
+    namespace_packages=[],
+    )
